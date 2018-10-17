c5b17d75a998a0506483cbb948155d755274f988
Matthew Martin <m.martin747@gmail.com>
2018-10-18 08:56:28 +1000

    sync master

--- extend_array.rb
+++ extend_array.rb
@@ -1,7 +1,20 @@
 class Array
   def square
+    map {|value| value * value} 
     # create your square method here
   end
-
+  def cube
+    map {|value| (value * value) * value}
+  end
+  def average
+    sum/length
+  end
+  def even
+  
+  end
   # now fill in the rest
+  array=[1,2,3,4,5]
+  array.average
 end
+
+
